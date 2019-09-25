a22215ad9c7f24a7ca3013f738106e54ad221b2c
DzSilkov <DzSilkov@gmail.com>
2019-09-26 01:23:48 +0300

    generate home and not found components

--- sport-base/src/app/app.module.ts
+++ sport-base/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes: Routes =[
+  { path: '', component: HomeComponent},
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
 
   imports: [
