4474dafcc07379dcd11f1abdd75a55a52f1c449a
DzSilkov <DzSilkov@gmail.com>
2019-09-26 00:54:07 +0300

    add home path routing

--- sport-base/src/app/app.module.ts
+++ sport-base/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes =[
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
